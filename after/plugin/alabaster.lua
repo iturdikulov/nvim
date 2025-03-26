3f8ae6d00ae375027f5c2adc8ad13578c2d05a3d
Inom Turdikulov <i@inomoz.ru>
2025-03-26 10:56:13 +0300

    alabaster: adjust colors for statusline and diff (avante)

--- after/plugin/alabaster.lua
+++ after/plugin/alabaster.lua
@@ -4,12 +4,17 @@ vim.cmd("colorscheme alabaster")
 vim.api.nvim_set_hl(0,'ColorColumn',{ bg='#30363f'})
 vim.api.nvim_set_hl(0,'CopilotAnnotation',{ fg='#9d9c9c'})
 vim.api.nvim_set_hl(0,'CopilotSuggestion',{ fg='#9d9c9c'})
+
 -- Visual selection
 vim.api.nvim_set_hl(0,'Visual',{ bg = '#333444'})
 
 -- Express line
-vim.api.nvim_set_hl(0,'ElInsert',{ fg='white', bg='green'})
+vim.api.nvim_set_hl(0,'ElInsert',{ fg='black', bg='yellow'})
 
 -- Transparent background
 vim.api.nvim_set_hl(0, 'Normal', { bg='NONE'})
 vim.api.nvim_set_hl(0, 'NonText', { bg='NONE', fg='#444a52'})
+
+-- Configure diff highlights
+vim.api.nvim_set_hl(0, 'DiffText',  { fg = "#341a00", bg = "#3F0001" })
+vim.api.nvim_set_hl(0, 'DiffAdd',  { fg = "#56d364", bg = "#244032" })
