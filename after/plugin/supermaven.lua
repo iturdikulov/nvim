f8dfbcbb76b2acc76454004b7b700c7d9feaff54
Inom Turdikulov <i@inomoz.ru>
2025-03-26 11:04:37 +0300

    supermaven: use default config, adjust keybinds

--- after/plugin/supermaven.lua
+++ after/plugin/supermaven.lua
@@ -5,7 +5,10 @@ supermaven.setup({
         suggestion_color = "#696969",
         cterm = 244,
     },
+    keymaps = {
+        accept_suggestion = "<C-f>",
+        clear_suggestion = "<C-]>",
+        accept_word = "<C-j>",
+    },
     log_level = "off",
-    disable_inline_completion = true,
-    disable_keymaps = true
 })
