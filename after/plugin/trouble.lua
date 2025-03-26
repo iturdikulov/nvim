7cb15e981c386d47f4d8abacaec7be7b9016d785
Inom Turdikulov <i@inomoz.ru>
2025-03-26 11:12:17 +0300

    trouble: refactor keymaps

--- after/plugin/trouble.lua
+++ after/plugin/trouble.lua
@@ -8,16 +8,10 @@ local map = function(lhs, rhs, desc)
     vim.keymap.set("n", lhs, rhs, {silent = true, desc = desc})
 end
 
-map("<leader>xx", function() require("trouble").toggle() end,
-    "Toggle")
-map("<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end,
-    "Workspace Diagnostics")
-map("<leader>xd",
-    function() require("trouble").toggle("document_diagnostics") end,
-    "Document Diagnostics")
-map("<leader>xq", function() require("trouble").toggle("quickfix") end,
-    "QuickFix")
-map("<leader>xl", function() require("trouble").toggle("loclist") end,
-    "LocList")
-map("gR", function() require("trouble").toggle("lsp_references") end,
-    "LSP references")
+map("<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", "Diagnostics")
+map("<leader>xX", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", "Buffer Diagnostics")
+map("<leader>cs", "<cmd>Trouble symbols toggle focus=false<cr>", "Symbols")
+map("<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", "LSP Definitions / references / ...")
+map("<leader>xL", "<cmd>Trouble loclist toggle<cr>", "Location List")
+map("<leader>xQ", "<cmd>Trouble qflist toggle<cr>", "Quickfix List")
+map("<leader>xt", "<cmd>Trouble todo<cr>", "Todo list")
