d175e6a6eea52cfb51069335156fe65921a26b37
Inom Turdikulov <i@inomoz.ru>
2025-03-26 10:55:14 +0300

    markdowny: use less common keybinds

--- after/plugin/makdowny.nvim.lua
+++ after/plugin/makdowny.nvim.lua
@@ -4,13 +4,13 @@ vim.api.nvim_create_autocmd('FileType', {
     desc = 'markdowny.nvim keymaps',
     pattern = {'markdown'},
     callback = function()
-        vim.keymap.set('v', '<C-b>', ":lua require('markdowny').bold()<cr>",
+        vim.keymap.set('v', '<M-b>', ":lua require('markdowny').bold()<cr>",
                        {buffer = 0})
-        vim.keymap.set('v', '<C-i>', ":lua require('markdowny').italic()<cr>",
+        vim.keymap.set('v', '<M-i>', ":lua require('markdowny').italic()<cr>",
                        {buffer = 0})
-        vim.keymap.set('v', '<C-k>', ":lua require('markdowny').link()<cr>",
+        vim.keymap.set('v', '<M-k>', ":lua require('markdowny').link()<cr>",
                        {buffer = 0})
-        vim.keymap.set('v', '<C-e>', ":lua require('markdowny').code()<cr>",
+        vim.keymap.set('v', '<M-e>', ":lua require('markdowny').code()<cr>",
                        {buffer = 0})
     end
 })
