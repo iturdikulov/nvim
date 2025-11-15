d8f8517d239dbd59a740fccdb8f8011ce642543b
Inom T. <i@inomoz.ru>
2025-11-15 20:42:38 +0300

    Switch default model

--- lua/plugins/minuet-ai.lua
+++ lua/plugins/minuet-ai.lua
@@ -37,7 +37,7 @@ return {
                     openai_compatible = {
                         api_key = "OPENROUTER_API_KEY",
                         end_point = "https://openrouter.ai/api/v1/chat/completions",
-                        model = "x-ai/grok-4-fast",
+                        model = "google/gemini-2.5-flash",
                         name = "Openrouter",
                         optional = {
                             max_tokens = 56,
