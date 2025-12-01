1846a2995d2c4ed34e027fea707f49949e27df7c
Inom T. <i@inomoz.ru>
2025-12-01 08:50:45 +0300

    Add sniprun integration

---
+++ lua/plugins/sniprun.lua
@@ -0,0 +1,16 @@
+return {
+    "michaelb/sniprun",
+    branch = "master",
+
+    build = "sh install.sh",
+    -- do 'sh install.sh 1' if you want to force compile locally
+    -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
+
+    config = function()
+        require("sniprun").setup({})
+
+        vim.keymap.set('v', '<leader>ee', '<Plug>SnipRun', {silent = true, desc="Sniprun: run code snippet with selection"})
+        vim.keymap.set('n', '<leader>ee', '<Plug>SnipRun', {silent = true, desc="Sniprun: run code snippet under cursor"})
+        vim.keymap.set('n', '<leader>eE', '<Plug>SnipRunOperator', {silent = true, desc="Sniprun: operator mode (text-objecs)"})
+    end,
+}
