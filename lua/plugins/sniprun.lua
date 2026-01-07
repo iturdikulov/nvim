ed1535b4261b21d6e1d5b7d50810b34603edc99e
Inom T. <i@inomoz.ru>
2026-01-07 10:12:38 +0300

    Sniprun: adjust formatting, add JS_TS_deno support

--- lua/plugins/sniprun.lua
+++ lua/plugins/sniprun.lua
@@ -7,10 +7,28 @@ return {
     -- (instead of fetching a binary from the github release). Requires Rust >= 1.65
 
     config = function()
-        require("sniprun").setup({})
+        require("sniprun").setup({
+            selected_interpreters = { "JS_TS_deno" },
+            repl_enable = { "JS_TS_deno" },
+        })
 
-        vim.keymap.set('v', '<leader>ee', '<Plug>SnipRun', {silent = true, desc="Sniprun: run code snippet with selection"})
-        vim.keymap.set('n', '<leader>ee', '<Plug>SnipRun', {silent = true, desc="Sniprun: run code snippet under cursor"})
-        vim.keymap.set('n', '<leader>eE', '<Plug>SnipRunOperator', {silent = true, desc="Sniprun: operator mode (text-objecs)"})
+        vim.keymap.set(
+            "v",
+            "<leader>ee",
+            "<Plug>SnipRun",
+            { silent = true, desc = "Sniprun: run code snippet with selection" }
+        )
+        vim.keymap.set(
+            "n",
+            "<leader>ee",
+            "<Plug>SnipRun",
+            { silent = true, desc = "Sniprun: run code snippet under cursor" }
+        )
+        vim.keymap.set(
+            "n",
+            "<leader>eE",
+            "<Plug>SnipRunOperator",
+            { silent = true, desc = "Sniprun: operator mode (text-objecs)" }
+        )
     end,
 }
