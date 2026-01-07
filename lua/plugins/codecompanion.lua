5bbb87779bdb15995f52557bf0452af5104f1689
Inom T. <i@inomoz.ru>
2026-01-07 10:18:37 +0300

    Adjust codecompanion setup, no history, change model

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -1,8 +1,8 @@
 return {
     "olimorris/codecompanion.nvim",
-    event = "VeryLazy",
     dependencies = {
-        "ravitemer/codecompanion-history.nvim",
+        "nvim-lua/plenary.nvim",
+        "nvim-treesitter/nvim-treesitter",
     },
     config = function()
         vim.keymap.set({ "n", "x" }, "<leader>ae", function()
@@ -15,43 +15,14 @@ return {
             vim.cmd("CodeCompanionChat Add")
         end, { desc = "Add selected code to chat" })
 
-        -- Expand cc to codecompanion in cmdline
         vim.cmd([[cab cc CodeCompanion]])
 
         require("codecompanion").setup({
-            extensions = {
-                history = {
-                    enabled = true,
-                    opts = {
-                        picker = "default",
-                    },
-                },
-            },
-            strategies = {
-                chat = { adapter = "gemini" },
-                inline = {
-                    adapter = "gemini",
-                },
-            },
-            adapters = {
-                http = {
-                    gemini = function()
-                        return require("codecompanion.adapters").extend(
-                            "gemini",
-                            {
-                                env = {
-                                    api_key = vim.env.GEMINI_API_KEY,
-                                },
-                                schema = {
-                                    model = {
-                                        default = "gemini-2.5-flash-lite", -- Use the name of your desired Ollama model
-                                    },
-                                },
-                            }
-                        )
-                    end,
-                },
+            interactions = {
+                chat = { adapter = "gemini_cli" },
+                inline = { adapter = "gemini_cli" },
             },
         })
     end,
 }
+
