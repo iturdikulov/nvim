1862c0bc5cddda1ec058a665ce054c8c2613f21b
Inom T. <i@inomoz.ru>
2026-03-28 18:39:41 +0300

    Status - add more items

--- lua/plugins/statusline.lua
+++ lua/plugins/statusline.lua
@@ -1,6 +1,8 @@
 return {
     "nvim-lualine/lualine.nvim",
     config = function()
+        local ok, _99 = pcall(require, "99")
+
         require("lualine").setup({
             options = {
                 icons_enabled = true,
@@ -8,7 +10,14 @@ return {
                 component_separators = "",
             },
             sections = {
-                lualine_b = { "branch", "diff", "diagnostics" },
+                lualine_b = {
+                    "branch",
+                    "diff",
+                    "diagnostics",
+                    function()
+                        return (ok and _99 and _99.get_model()) or ""
+                    end,
+                },
             },
         })
     end,
